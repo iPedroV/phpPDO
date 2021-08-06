1eae6910e8d46968225bcc8c1826794bd871f51f
Pedro <pedro.vcosta405@gmail.com>
2021-08-06 11:35:56 -0300

    [UPDATE] Funcionando até aqui :ok_hand:

--- controller/PessoaController.php
+++ controller/PessoaController.php
@@ -2,19 +2,33 @@
 require_once "C:/xampp/htdocs/phpPDO/dao/daoPessoa.php";
 require_once 'C:/xampp/htdocs/phpPDO/model/Pessoa.php';
 require_once 'C:/xampp/htdocs/phpPDO/model/Endereco.php';
-class PessoaController {
+class PessoaController
+{
 
-    public function inserirPessoa($nome, $dtNasc, $login, $senha, 
-            $perfil, $email, $cpf, $cep, $logradouro , $uf , $bairro, $cidade, $complemento ){
-        
-        $endereco = new Endereco(); 
+    public function inserirPessoa(
+        $nome,
+        $dtNasc,
+        $login,
+        $senha,
+        $perfil,
+        $email,
+        $cpf,
+        $cep,
+        $logradouro,
+        $uf,
+        $bairro,
+        $cidade,
+        $complemento
+    ) {
+
+        $endereco = new Endereco();
         $endereco->setCep($cep);
         $endereco->setLogradouro($logradouro);
         $endereco->setComplemento($complemento);
         $endereco->setBairro($bairro);
         $endereco->setCidade($cidade);
         $endereco->setUf($uf);
-        
+
         $pessoa = new Pessoa();
         $pessoa->setNome($nome);
         $pessoa->setDtNasc($dtNasc);
@@ -28,5 +42,63 @@ class PessoaController {
 
         $daoPessoa = new daoPessoa();
         return $daoPessoa->inserir($pessoa);
+    }
+
+    public function listarPessoas()
+    {
+        $daoPessoa = new daoPessoa();
+        return $daoPessoa->listarPessoasDAO();
+    }
+
+    public function atualizarPessoa(
+        $id,
+        $nome,
+        $dtNasc,
+        $login,
+        $senha,
+        $perfil,
+        $email,
+        $cpf,
+        $cep,
+        $logradouro,
+        $complemento,
+        $bairro,
+        $cidade,
+        $uf
+    ) {
+        $endereco = new Endereco();
+        $endereco->setCep($cep);
+        $endereco->setLogradouro($logradouro);
+        $endereco->setComplemento($complemento);
+        $endereco->setBairro($bairro);
+        $endereco->setCidade($cidade);
+        $endereco->setUf($uf);
+        $pessoa = new Pessoa();
+
+        $pessoa->setIdPessoa($id);
+        $pessoa->setNome($nome);
+        $pessoa->setDtNasc($dtNasc);
+        $pessoa->setLogin($login);
+        $pessoa->setSenha($senha);
+        $pessoa->setPerfil($perfil);
+        $pessoa->setEmail($email);
+        $pessoa->setCpf($cpf);
+
+        $pessoa->setFkEndereco($endereco);
+
+
+        $daoPessoa = new daoPessoa();
+        return $daoPessoa->atualizarPessoaDAO($pessoa);
+    }
+
+    public function pesquisarPessoaID($id)
+    {
+        $daoProduto = new daoPessoa();
+        return $daoProduto->pesquisarPessoaIdDAO($id);
+    }
+
+    public function excluirPessoa($id){
+        $daoP = new DaoPessoa();
+        return $daoP->excluirPessoaDAO($id);
     }
 }
--- dao/daoPessoa.php
+++ dao/daoPessoa.php
@@ -26,10 +26,10 @@ class daoPessoa
 
             $cep = $pessoa->getFkEndereco()->getCep();
             $logradouro = $pessoa->getFkEndereco()->getLogradouro();
-            $complemento = $pessoa->getFkEndereco()->getComplemento();
+            $uf = $pessoa->getFkEndereco()->getUf();
             $bairro = $pessoa->getFkEndereco()->getBairro();
             $cidade = $pessoa->getFkEndereco()->getCidade();
-            $uf = $pessoa->getFkEndereco()->getUf();
+            $complemento = $pessoa->getFkEndereco()->getComplemento();
 
             try {
                 //processo para pegar o idendereco da tabela endereco, conforme 
@@ -50,7 +50,7 @@ class daoPessoa
                     } else {
                         $st2 = $conecta->prepare("insert into "
                             . "endereco values (null,?,?,?,?,?,?)");
-                            //TEM QUE ESTAR NA MESMA ORDEM DO BANCO DE DADOS
+                        //TEM QUE ESTAR NA MESMA ORDEM DO BANCO DE DADOS
                         $st2->bindParam(1, $cep);
                         $st2->bindParam(2, $logradouro);
                         $st2->bindParam(3, $uf);
@@ -102,5 +102,221 @@ class daoPessoa
         }
         $conn = null;
         return $msg;
+    }
+
+    public function listarPessoasDAO()
+    {
+        $msg = new Mensagem();
+        $conn = new Conecta();
+        $conecta = $conn->conectadb();
+        if ($conecta) {
+            try {
+                $rs = $conecta->query("SELECT * FROM pessoa inner join endereco "
+                . "on pessoa.fkEndereco = endereco.idEndereco ");
+                $lista = array();
+                $a = 0;
+                if ($rs->execute()) {
+                    if ($rs->rowCount() > 0) {
+                        while ($linha = $rs->fetch(PDO::FETCH_OBJ)) {
+                            $pessoa = new Pessoa();
+                            $pessoa->setIdPessoa($linha->idpessoa);
+                            $pessoa->setNome($linha->nome);
+                            $pessoa->setDtNasc($linha->dtNasc);
+                            $pessoa->setLogin($linha->login);
+                            $pessoa->setSenha($linha->senha);
+                            $pessoa->setPerfil($linha->perfil);
+                            $pessoa->setEmail($linha->email);
+                            $pessoa->setCpf($linha->cpf);
+
+                            $endereco = new Endereco();
+                            $endereco->setIdEndereco($linha->idEndereco);
+                            $endereco->setCep($linha->cep);
+                            $endereco->setLogradouro($linha->logradouro);
+                            $endereco->setUf($linha->uf);
+                            $endereco->setBairro($linha->bairro);
+                            $endereco->setCidade($linha->cidade);
+                            $endereco->setComplemento($linha->complemento);
+
+                            $pessoa->setFkEndereco($endereco); 
+                            $lista[$a] = $pessoa;
+                            $a++;
+                        }
+                    }
+                }
+            } catch (Exception $ex) {
+                $msg->setMsg($ex);
+            }
+            $conn = null;
+            return $lista;
+        }
+    }
+
+    public function atualizarPessoaDAO(Pessoa $pessoa){
+        $conn = new Conecta();
+        $msg = new Mensagem();
+        $conecta = $conn->conectadb();
+        if ($conecta) {
+            $idPessoa = $pessoa->getIdpessoa();
+            $nome = $pessoa->getNome();
+            $dtNasc = $pessoa->getDtNasc();
+            $login = $pessoa->getLogin();
+            $senha = $pessoa->getSenha();
+            $perfil = $pessoa->getPerfil();
+            $email = $pessoa->getEmail();
+            $cpf = $pessoa->getCpf();
+
+            $cep = $pessoa->getFkEndereco()->getCep();
+            $logradouro = $pessoa->getFkEndereco()->getLogradouro();
+            $complemento = $pessoa->getFkEndereco()->getComplemento();
+            $bairro = $pessoa->getFkEndereco()->getBairro();
+            $cidade = $pessoa->getFkEndereco()->getCidade();
+            $uf = $pessoa->getFkEndereco()->getUf();
+
+            try {
+                //processo para pegar o idendereco da tabela endereco, conforme 
+                //o cep, o logradouro e o complemento informado.
+                $st = $conecta->prepare("select idEndereco "
+                    . "from endereco where cep = ? and "
+                    . "logradouro = ? and complemento = ? limit 1");
+                $st->bindParam(1, $cep);
+                $st->bindParam(2, $logradouro);
+                $st->bindParam(3, $complemento);
+                if ($st->execute()) {
+                    if ($st->rowCount() > 0) {
+                        //$msg->setMsg("".$st->rowCount());
+                        while ($linha = $st->fetch(PDO::FETCH_OBJ)) {
+                            $fkEnd = $linha->idEndereco;
+                        }
+                        //$msg->setMsg("$fkEnd");
+                    } else {
+                        $st2 = $conecta->prepare("insert into "
+                            . "endereco values (null,?,?,?,?,?,?)");
+                            //TEM QUE ESTAR NA MESMA ORDEM DO BANCO DE DADOS
+                        $st2->bindParam(1, $cep);
+                        $st2->bindParam(2, $logradouro);
+                        $st2->bindParam(3, $uf);
+                        $st2->bindParam(4, $bairro);
+                        $st2->bindParam(5, $cidade);       
+                        $st2->bindParam(6, $complemento);
+                        $st2->execute();    
+                        $st3 = $conecta->prepare("select idEndereco "
+                            . "from endereco where cep = ? and "
+                            . "logradouro = ? and complemento = ? limit 1");
+                        $st3->bindParam(1, $cep);
+                        $st3->bindParam(2, $logradouro);
+                        $st3->bindParam(3, $complemento);
+                        if ($st3->execute()) {
+                            if ($st3->rowCount() > 0) {
+                                $msg->setMsg("" . $st3->rowCount());
+                                while ($linha = $st3->fetch(PDO::FETCH_OBJ)) {
+                                    $fkEnd = $linha->idEndereco;
+                                }
+                                //$msg->setMsg("$fkEnd");
+                            }
+                        }
+                    }
+                    $stmt = $conecta->prepare("update pessoa set "
+                        . "nome = ?, dtNasc = ?, login = ?, senha = ?, "
+                        . "perfil = ?, email = ?, cpf = ?, fkEndereco = ? "
+                        . "where idPessoa = ?");
+
+                    $stmt->bindParam(1, $nome);
+                    $stmt->bindParam(2, $dtNasc);
+                    $stmt->bindParam(3, $login);
+                    $stmt->bindParam(4, $senha);
+                    $stmt->bindParam(5, $perfil);
+                    $stmt->bindParam(6, $email);
+                    $stmt->bindParam(7, $cpf);
+                    $stmt->bindParam(8, $fkEnd);
+                    $stmt->bindParam(9, $idPessoa);
+
+                    $stmt->execute();
+                } 
+
+            } catch (Exception $ex) {
+                $msg->setMsg($ex);
+            } 
+            } else {
+                $msg->setMsg("<p style='color: red;'>"
+                    . "Erro na conexão com o banco de dados.</p>");
+            }
+            $conn = null;
+            return $msg;
+    }
+
+    public function pesquisarPessoaIdDAO($id){
+        $msg = new Mensagem();
+        $conn = new Conecta();
+        $conecta = $conn->conectadb();
+        $pessoa = new Pessoa();
+        if($conecta){
+            try {
+                
+                $msg->setMsg("<p style='color: green;'>"
+                    . "Dados Cadastrados com sucesso</p>");
+                $rs = $conecta->prepare("SELECT * FROM pessoa inner join endereco "
+                . "on pessoa.fkEndereco = endereco.idEndereco where pessoa.idpessoa = ? limit 1");
+                $rs->bindParam(1, $id);
+                if($rs->execute()){
+                    if($rs->rowCount() > 0){
+                        $endereco = new Endereco();
+                        while($linha = $rs->fetch(PDO::FETCH_OBJ)){
+                            $pessoa->setIdPessoa($linha->idpessoa);
+                            $pessoa->setNome($linha->nome);
+                            $pessoa->setDtNasc($linha->dtNasc);
+                            $pessoa->setLogin($linha->login);
+                            $pessoa->setSenha($linha->senha);
+                            $pessoa->setPerfil($linha->perfil);
+                            $pessoa->setEmail($linha->email);
+                            $pessoa->setCpf($linha->cpf);
+
+                            
+                            $endereco->setIdEndereco($linha->idEndereco);
+                            $endereco->setCep($linha->cep);
+                            $endereco->setLogradouro($linha->logradouro);
+                            $endereco->setUf($linha->uf);
+                            $endereco->setBairro($linha->bairro);
+                            $endereco->setCidade($linha->cidade);
+                            $endereco->setComplemento($linha->complemento);
+
+                            $pessoa->setFkEndereco($endereco); 
+                            
+                            
+                        }
+                    }
+                }
+            } catch (Exception $ex) {
+                $msg->setMsg($ex);
+            }  
+            $conn = null;
+        }else{
+            echo "<script>alert('Banco inoperante!')</script>";
+            echo "<META HTTP-EQUIV='REFRESH' CONTENT=\"0;
+			 URL='../phpPDO/cadastro.php'\">"; 
+        }
+        return $pessoa;
+    }
+
+    public function excluirPessoaDAO($id){
+        $conn = new Conecta();
+        $conecta = $conn->conectadb();
+        $msg = new Mensagem();
+        if($conecta){
+            try {
+                $stmt = $conecta->prepare("delete from pessoa "
+                        . "where idPessoa = ?");
+                $stmt->bindParam(1, $id);
+                $stmt->execute();
+                $msg->setMsg("<p style='color: #d6bc71;'>"
+                        . "Dados excluídos com sucesso.</p>");
+            } catch (Exception $ex) {
+                $msg->setMsg($ex);
+            }
+        }else{
+            $msg->setMsg("<p style='color: red;'>'Banco inoperante!'</p>"); 
+        }
+        $conn = null;
+        return $msg;
+
     }
 }
